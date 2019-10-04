dbc5bf75b621ab9ab0058dc9e39a7bec06734b53
ankundn <ankundanisha0@gmail.com>
2019-10-04 14:21:38 +0300

    Added Models, Serializers,Registered

--- refactory/serializers.py
+++ refactory/serializers.py
@@ -1,1 +1,57 @@
 from rest_framework import serializers
+
+from .models import Cohort
+from .models import Catalyst
+from .models import Bootcamp
+from .models import Contact
+from .models import EducationBackground
+from .models import SkillSet
+from .models import Honor
+from .models import ApplicationVideo
+from .models import Interview
+
+
+class CohortSerializer (serializers.HyperlinkedModelSerializer) :
+    class Meta:
+        model = Cohort
+        fields = ('cohort_id', 'name_name', 'registration_date', 'start_date', 'end_date', 'cohort_number', 'description', 'running')
+
+class CatalystSerializer (serializers.HyperlinkedModelSerializer) :
+    class Meta:
+        model = Catalyst
+        fields = ('catalyst_id', 'name_name', 'registration_date', 'start_date', 'end_date', 'catalyst_number', 'description', 'running')
+
+class BootcampSerializer (serializers.HyperlinkedModelSerializer) :
+    class Meta:
+        model = Bootcamp
+        fields = ('bootcamp_id', 'name_name', 'registration_date', 'start_date', 'end_date', 'bootcamp_number', 'description', 'running')
+
+class ContactSerializer (serializers.HyperlinkedModelSerializer) :
+    class Meta:
+        model = Contact
+        fields = ('applicant_id', 'linkedin_link', 'twitter_link', 'facebook_link')
+
+class EducationBackgroundSerializer (serializers.HyperlinkedModelSerializer) :
+    class Meta:
+        model = EducationBackground
+        fields =('applicant_id', 'school', 'degree', 'Field_of_study', 'grade', 'From', 'to', 'Extra_activity')
+
+class SkillSetSerializer (serializers.HyperlinkedModelSerializer) :
+    class Meta: SkillSet
+    fields = ('skill_id', 'skill_name', 'description')
+
+class HonorSerializer (serializers.HyperlinkedModelSerializer):
+    class Meta: Honor
+    fields = ('applicant_id', 'honor_title', 'association', 'Issuer', 'date_of_honor', 'Description')
+
+class ApplicationVideoSerializer (serializers.HyperlinkedModelSerializer):
+    class Meta: ApplicationVideo
+    fields = ('application_id', 'videolink')
+
+class InterviewSerializer (serializers.HyperlinkedModelSerializer):
+    class Meta: Interview
+    fields = ('schedule_id', 'interview_id', 'start_date', 'end_date')
+
+    
+
+    
--- refactory/urls.py
+++ refactory/urls.py
@@ -3,7 +3,32 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from refactory import views
 from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register(r'cohort', views.CohortViewSet)
 
+router1 = routers.DefaultRouter()
+router1.register(r'catalyst', views.CatalystViewSet)
+
+router2 = routers.DefaultRouter()
+router2.register(r'bootcamp', views.BootcampViewSet)
+
+router3 = routers.DefaultRouter()
+router3.register(r'contact', views.ContactViewSet)
+
+router4 = routers.DefaultRouter()
+router4.register(r'EducationBackground', views.EducationBackgroundViewSet)
+
+router5 = routers.DefaultRouter()
+router5.register(r'SkillSet', views.SkillSetViewSet)
+
+router6 = routers.DefaultRouter()
+router6.register(r'honor', views.HonorViewSet)
+
+router7 = routers.DefaultRouter()
+router7.register(r'ApplicationVideo', views.ApplicationVideoViewSet)
+
+router8 = routers.DefaultRouter()
+router8.register(r'Interview', views.InterviewViewSet)
 urlpatterns = [
     
     
